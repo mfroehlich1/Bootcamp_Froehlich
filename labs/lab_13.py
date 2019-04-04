e5084ef62218d81d6480bc8bd289ed561d4ed96b
Mitch Froehlich <mitchel.froehlich@gmail.com>
2019-04-04 14:24:11 -0700

    improved lab_13.py removed unnessecary code

--- labs/lab_13.py
+++ labs/lab_13.py
@@ -1,34 +1,22 @@
-import string
 
 
 def rot13(message_input):
     alphabet_dict = {}
-    punctuation = string.punctuation
-    punctuation_list = []
-
-    for item in punctuation:
-        punctuation_list.append(item)
 
     for i in range(26):
         alphabet_dict[i] = chr(i + 97)
     cipher_list = []
 
     for char in message_input:
-        if char != " ":
-            if ord(char) >= 97 and ord(char) <= 122:
-                alphabet_num = (ord(char))
-                cipher_num = ((alphabet_num -97) + 13) % 26
-                cipher_list.append(chr(cipher_num + 97))
-            if ord(char) >= 65 and ord(char) <= 90:
-                alphabet_num = (ord(char))
-                cipher_num = ((alphabet_num - 65) + 13) % 26
-                cipher_list.append(chr(cipher_num + 65))
-        if char == ' ':
-            cipher_list.append(char)
-        for item in punctuation_list:
-            if item == char:
-                cipher_list.append(char)
-        if char.isdigit():
+        if ord(char) >= 97 and ord(char) <= 122:
+            alphabet_num = (ord(char))
+            cipher_num = ((alphabet_num - 97) + 13) % 26
+            cipher_list.append(chr(cipher_num + 97))
+        elif ord(char) >= 65 and ord(char) <= 90:
+            alphabet_num = (ord(char))
+            cipher_num = ((alphabet_num - 65) + 13) % 26
+            cipher_list.append(chr(cipher_num + 65))
+        else:
             cipher_list.append(char)
 
     cipher_message = ''.join(cipher_list)
