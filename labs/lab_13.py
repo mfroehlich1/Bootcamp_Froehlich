b4ea0d3ea0338c9afb8d6a8225ff43c4c87524c7
Mitch Froehlich <mitchel.froehlich@gmail.com>
2019-04-04 18:16:11 -0700

    added some comments to lab_13

--- labs/lab_13.py
+++ labs/lab_13.py
@@ -3,20 +3,20 @@
 def rot13(message_input):
     alphabet_dict = {}
 
-    for i in range(26):
+    for i in range(26): # generates my dictionary of the alphabet
         alphabet_dict[i] = chr(i + 97)
     cipher_list = []
 
     for char in message_input:
-        if ord(char) >= 97 and ord(char) <= 122:
+        if ord(char) >= 97 and ord(char) <= 122: # matches lowercase letters
             alphabet_num = (ord(char))
             cipher_num = ((alphabet_num - 97) + 13) % 26
             cipher_list.append(chr(cipher_num + 97))
-        elif ord(char) >= 65 and ord(char) <= 90:
+        elif ord(char) >= 65 and ord(char) <= 90: # matches capital letters
             alphabet_num = (ord(char))
             cipher_num = ((alphabet_num - 65) + 13) % 26
             cipher_list.append(chr(cipher_num + 65))
-        else:
+        else: # anything that isn't matched above is kept as-is and added to list
             cipher_list.append(char)
 
     cipher_message = ''.join(cipher_list)
