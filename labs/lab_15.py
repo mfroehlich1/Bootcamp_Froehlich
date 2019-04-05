b6a9697869307530489fa83080c7689e2793024c
Mitch Froehlich <mitchel.froehlich@gmail.com>
2019-04-04 20:26:56 -0700

    adding comments

--- labs/lab_15.py
+++ labs/lab_15.py
@@ -38,20 +38,20 @@ ones_dict = {
 
 user_input = int(input("Using numbers, pick a number between 0-99: "))
 
-if user_input == 0:
+if user_input == 0: #  if 0 we just print 0 and we're done
     print("zero")
 
-elif user_input in teens_dict:
+elif user_input in teens_dict: #  if in the 10s we have to account for english weirdness
     for key in teens_dict:
         if key == user_input:
             print(teens_dict[key])
 
-elif user_input in ones_dict:
+elif user_input in ones_dict: #  accounting for single digits as well
     for key in ones_dict:
         if key == user_input:
             print(ones_dict[key])
 
-else:
+else: #  every other number choice should match here
     tens_digit = user_input // 10
     ones_digit = user_input % 10
 
